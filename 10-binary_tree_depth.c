52018d8694629914a05872202119b08c40af8b2a
dontstopmeowing <dnny419@gmail.com>
2021-07-22 05:37:33 +0700

    Fix?

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	for (size_t i = 0; tree->parent != NULL; i++)
+	while (tree->parent != NULL)
 	{
 		counter++;
 		tree = tree->parent;
