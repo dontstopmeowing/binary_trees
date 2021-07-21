117f7c3189438ffd5e160354bff194ba04974231
dontstopmeowing <dnny419@gmail.com>
2021-07-22 05:36:19 +0700

    Adding task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - function that measures
+ * the depth of a node in a binary tree
+ * @tree: pointer to the root node of the tree to traverse
+ * Return: the depth of a node in a binary tree
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t counter = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	for (size_t i = 0; tree->parent != NULL; i++)
+	{
+		counter++;
+		tree = tree->parent;
+	}
+
+	return (counter);
+}
