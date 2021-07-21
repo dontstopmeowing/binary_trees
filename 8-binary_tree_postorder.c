6ea9cc04e4ece82c989f78e16b4ed85d1877b828
dontstopmeowing <dnny419@gmail.com>
2021-07-22 04:19:36 +0700

    Fix

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through
- * a binary tree using in-order traversal traversal
+ * binary_tree_postorder - function that goes through
+ * a binary tree using post-order traversal traversal
  *
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
